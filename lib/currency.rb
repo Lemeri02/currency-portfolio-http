041fbd966746fdc294dcb1cfc5956fbc416f01b2
Lemeri02 <9588504@gmail.com>
2020-01-05 21:47:54 +0500

    refactored class currency with method map instead of each

--- lib/currency.rb
+++ lib/currency.rb
@@ -6,17 +6,16 @@ class Curency
   end
 
   def self.from_xml(node)
-    units = []
-
-    node.elements.each('ValCurs/Valute') do |element|
-      valute = { id: element.attributes['ID'],
-                 char_code: element.elements['CharCode'].text,
-                 value: element.elements['Value']
-                               .text.sub(',', '.').to_f.round(4) }
-      units << valute
-    end
-
-    new(units.map { |unit| CurrencyUnit.new(unit) })
+    new(
+      node.elements['ValCurs'].map do |element|
+        CurrencyUnit.new(
+          id: element.attributes['ID'],
+          char_code: element.elements['CharCode'].text,
+          value: element.elements['Value']
+                        .text.sub(',', '.').to_f.round(2)
+        )
+      end
+    )
   end
 
   def unit
--- main.rb
+++ main.rb
@@ -10,7 +10,6 @@ uri = URI.parse('http://www.cbr.ru/scripts/XML_daily.asp')
 begin
   response = Net::HTTP.get_response(uri)
 rescue SocketError => e
-  puts "Похоже, Интернет-соединение было прервано (#{e.class})"
   puts e.message
   exit
 end
@@ -21,14 +20,14 @@ currency = Curency.from_xml(doc)
 
 unit = currency.unit
 
-puts 'Бивалютный портфель. v.2.0.0'
+puts 'Бивалютный портфель. v.2.0.1'
 puts "Курс #{unit.char_code}: #{unit.value}"
 
 puts 'Сколько у вас рублей?'
-rubs = gets.to_f
+rubs = STDIN.gets.to_f
 
 puts 'Сколько у вас долларов?'
-dollars = gets.to_f
+dollars = STDIN.gets.to_f
 
 budget = Budget.new(unit.value, rubs, dollars)
 
